dd9e6b7b48c3f14e30ca6a20dee4d4b78ffccf62
callsamu <samuelbree@gmail.com>
2021-11-17 13:51:19 +0000

    Use PATH_INFO key for getting the uri

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 require_once __DIR__.'/../vendor/autoload.php';
 use Samu\TodoList\Controllers\FrontController;
 
-$uri = $_SERVER['REQUEST_URI'];
+$uri = $_SERVER['PATH_INFO'];
 (new FrontController)->processRequest($uri);
 
 
