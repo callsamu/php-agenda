2f8f7a2cc4f576bec15a88f4e5d3ef031f84aba4
callsamu <samuelbree@gmail.com>
2021-11-23 14:36:43 +0000

    Move FrontController to index.php; use dependency injection.

--- public/index.php
+++ public/index.php
@@ -1,16 +1,19 @@
 <?php
-
+error_reporting(E_ALL);
 require_once __DIR__.'/../vendor/autoload.php';
-use Samu\TodoList\Controllers\FrontController;
+$configDirectory = __DIR__ . '/../config/';
+
+$routes = require $configDirectory.'routes.php';
+$container = require $configDirectory.'container.php';
 
 $uri = $_SERVER['PATH_INFO'];
-(new FrontController)->processRequest($uri);
 
+if (!array_key_exists($uri, $routes)) {
+    http_response_code(404);
+    exit();
+}
 
+$controllerClass = $routes[$uri];
+$controller = new $controllerClass($container);
 
-    
-
-    
-
-
-
+$controller->processRequest($uri);
