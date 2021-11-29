3bfab36f2cc3256c408a98019bc26d000b62cf62
callsamu <samuelbree@gmail.com>
2021-11-29 14:46:06 +0000

    Refactor to reflect changes on Task Model and discontinue ::merge() method for updating.

--- src/Controllers/Persistance.php
+++ src/Controllers/Persistance.php
@@ -13,20 +13,57 @@ use Samu\TodoList\Entity\Task;
 class Persistance
 implements RequestHandlerInterface
 {
+    private array $postParameters;
+
     public function __construct(
         private EntityManagerInterface $entityManager
     ) {}
+    
+    public function error()
+    {
+        // TODO: Implement flash message
+
+        return '/new-task';
+        echo "hahaha";
+    }
+    
+    public function create()
+    {
+        extract($this->postParameters);
+
+        $task = new Task(
+            $description,
+            new DateTime($schedule), 
+            new DateTime($deadline)
+        );
+
+        $this->entityManager->persist($task);
+        $this->entityManager->flush();
+
+        echo "hahaha";
+        
+        return '/tasks';
+    }
+
+    public function update (int $id): string
+    {
+        extract($this->postParameters);
+
+        $task = $this->entityManager->find(Task::class, $id);
+        
+        $task->updateName($description);
+        $task->updateDates(new DateTime($schedule), new DateTime($deadline));
+
+        $this->entityManager->flush();
+        
+        return '/tasks';
+    }
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        extract($request->getParsedBody());
-
-        $name = filter_var($description, FILTER_SANITIZE_STRING);
-            
-        $task = new Task(
-            $name,
-            new DateTime($schedule),
-            new DateTime($deadline)
+        $this->postParameters = filter_var_array(
+            $request->getParsedBody(),
+            FILTER_SANITIZE_STRING
         );
         
         $id = filter_var(
@@ -34,16 +71,12 @@ implements RequestHandlerInterface
             FILTER_VALIDATE_INT
         );
 
-        if ($id === null || $id === false) {
-            $this->entityManager->persist($task);
-        } else {
-            var_dump($task);
-            $task->setId($id);
-            $this->entityManager->merge($task);
-        }
+        $redirect = match($id) {
+            true => $this->update($id),
+            false => $this->create(),
+            null => $this->error()
+        };
         
-        $this->entityManager->flush();
-
-        return new Response(200, ['Location' => '/tasks']);
+        return new Response(200, ['Location' => $redirect]);
     }
 }
